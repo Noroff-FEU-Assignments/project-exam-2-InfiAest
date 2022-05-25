a5cf2c7f17ff83c4e68592c272d1f0dd7775a5c0
Charlotte Lucas <charlotte.lucas@hotmail.com>
2022-05-25 18:08:11 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,10 @@
 import "../styles/sass/style.scss";
-import Script from "next/script";
 import { AuthProvider } from "../context/AuthContext";
 import Footer from "../components/layout/general/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Script
-        src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"
-        strategy="beforeInteractive"
-      />
       <div className="wrapper">
         <Component {...pageProps} />
       </div>
